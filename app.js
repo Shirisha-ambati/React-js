6769de4c8e675f61686b5c85e7f268306284fcd0
shirisha <shirishaambati987@gmail.com>
2026-04-27 19:34:17 +0530

    App.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "hiiee siri!!"),
-  ),
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement("h1", { className: "child1" }, "Siri");
+const heading2 = React.createElement("h2", { className: "child2" }, "Reddy");
+const parent = React.createElement("div", { id: "parent" }, [
+  heading1,
+  heading2,
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
---
+++ dist/Namaste_React.7c0ccee6.js
@@ -0,0 +1,9 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, React.createElement("h1", {}, "hiiee siri!!")));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=Namaste_React.7c0ccee6.js.map
